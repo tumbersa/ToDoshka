c36cde25352f357a69e03e1f0c127549171c3626
Gleb Kapustin <gleb.kapustin.95@gmail.com>
2024-06-19 23:05:54 +0300

    Initial Commit

---
+++ ToDoshka/ToDoshkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoshkaApp.swift
+//  ToDoshka
+//
+//  Created by Глеб Капустин on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoshkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
